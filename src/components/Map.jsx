d3bd6155c8942a0d54a0594222a6ed53c9f6badc
abh1sh3kk <abhishekacharya171@gmail.com>
2023-11-01 18:41:14 +0545

    change marker position on click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,24 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvent,
+} from "react-leaflet";
 
 const Map = () => {
+  const [markerPosition, setMarkerPosition] = useState([
+    27.700379692577663, 85.35719108777057,
+  ]);
+
+  const CustomMarker = () => {
+    useMapEvent("click", (e) =>
+      setMarkerPosition([e.latlng.lat, e.latlng.lng])
+    );
+    return null;
+  };
+
   return (
     <MapContainer
       className="map"
@@ -12,7 +30,8 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[27.700379692577663, 85.35719108777057]}>
+      <CustomMarker />
+      <Marker position={markerPosition}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
